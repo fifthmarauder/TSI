ca2f33d15c50864b9b8dfefe83a42025d442c6ee
fifthmarauder <eeshashahbaz12345@gmail.com>
2025-11-20 17:58:15 +0500

    fixed alignment of progress bar

--- src/components/OurServices/OurServices.tsx
+++ src/components/OurServices/OurServices.tsx
@@ -4,11 +4,8 @@ import styles from "./ourServices.module.css";
 import Headings from "../Common/Headings/Headings";
 import EmojiEventsOutlinedIcon from "@mui/icons-material/EmojiEventsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
+import ProgressBar from "@/components/Common/ProgressBar/ProgressBar";
 import "react-circular-progressbar/dist/styles.css";
-import AnimatedProgressProvider from "@/components/Common/AnimatedProgressProvider/AnimatedProgressProvider";
-import { easeQuadInOut } from "d3-ease";
-import { colors } from "@mui/material";
 
 const OurServices = () => {
   return (
@@ -33,33 +30,7 @@ const OurServices = () => {
                 <div className={styles.successContainerTitle}>
                   Project Success Rate
                 </div>
-                <div>
-                  <AnimatedProgressProvider
-                    valueStart={0}
-                    valueEnd={89}
-                    duration={1}
-                    easingFunction={easeQuadInOut}
-                    repeat={false}
-                  >
-                    {(value) => {
-                      const roundedValue = Math.round(value);
-                      return (
-                        <CircularProgressbar
-                          value={value}
-                          text={`${roundedValue}%`}
-                          /* This is important to include, because if you're fully managing the
-        animation yourself, you'll want to disable the CSS animation. */
-                          styles={buildStyles({
-                            pathTransition: "none",
-                            pathColor: "#C01C25",
-                            textColor: "black",
-                            textSize: 15,
-                          })}
-                        />
-                      );
-                    }}
-                  </AnimatedProgressProvider>
-                </div>
+                <ProgressBar />
               </div>
             </div>
           </div>
